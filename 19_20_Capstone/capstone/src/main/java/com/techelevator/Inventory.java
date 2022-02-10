a5d450bf8066b5384d98843b5972a34561f43a68
Grace Lefever <gracelefever2214@gmail.com>
2022-02-10 13:42:00 -0500

    Inventory Class created

---
+++ 19_20_Capstone/capstone/src/main/java/com/techelevator/Inventory.java
@@ -0,0 +1,52 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public abstract class Inventory {
+
+    private String name;
+    private BigDecimal price;
+    private int numberRemaining = 5;
+    private int numberPurchased = 0;
+
+    //constructor
+
+    public void Inventory(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //getters
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getNumberRemaining() {
+        return numberRemaining;
+    }
+
+    public int getNumberPurchased() {
+        return numberPurchased;
+    }
+
+    //setters
+
+    public void setNumberRemaining(int numberRemaining) {
+        this.numberRemaining = numberRemaining;
+    }
+
+    public void setNumberPurchased(int numberPurchased) {
+        this.numberPurchased = numberPurchased;
+    }
+
+    //method
+
+    public abstract String purchased();
+
+
+}
